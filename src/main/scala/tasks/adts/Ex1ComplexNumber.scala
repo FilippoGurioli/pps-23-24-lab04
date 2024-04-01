1677819e6f995e507d5780b86873eef3b87ea6c2
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2024-04-01 10:45:42 +0200

    Complete task 1, dunno if tests passes because they do not "record any output"

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -22,11 +22,16 @@ object Ex1ComplexNumbers:
   object BasicComplexADT extends ComplexADT:
 
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    private case class ComplexNumber(re: Double, im: Double)
+    opaque type Complex = ComplexNumber
+    def complex(re: Double, im: Double): Complex = ComplexNumber(re, im)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex.re
+      def im(): Double = complex.im
+      def sum(other: Complex): Complex = ComplexNumber(complex.re + other.re, complex.im + complex.im)
+      def subtract(other: Complex): Complex = ComplexNumber(complex.re - other.re, complex.im - complex.im)
+      def asString(): String = complex match
+        case ComplexNumber(re, im) if re != 0 && im != 0 => re + (if im > 0 then " + " else " - ") + im + "i"
+        case ComplexNumber(re, im) if im == 0 => re + ""
+        case ComplexNumber(re, im) if re == 0 => im + "i"
+      
