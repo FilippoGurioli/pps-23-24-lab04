ac1262731137f5dc1f731aa09b5f8ed18bdbd92f
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2024-04-01 12:17:42 +0200

    Kinda completed ex2 without tests. I do know how to implement but don't know how to use

--- src/main/scala/tasks/adts/Ex2SchoolModel.scala
+++ src/main/scala/tasks/adts/Ex2SchoolModel.scala
@@ -1,7 +1,6 @@
 package tasks.adts
 import u03.Sequences.*
 import u03.Optionals.*
-import u02.AlgebraicDataTypes.Person
 
 /*  Exercise 2: 
  *  Implement the below trait, and write a meaningful test.
@@ -28,3 +27,46 @@ object SchoolModel:
       def nameOfCourse(course: Course): String
       def setTeacherToCourse(teacher: Teacher, course: Course): School
       def coursesOfATeacher(teacher: Teacher): Sequence[Course]
+
+  object SchoolModuleImpl extends SchoolModule:
+    case class CourseImpl(name: String)
+    opaque type Course = CourseImpl
+
+    case class TeacherImpl(name: String, courses: Sequence[Course])
+    opaque type Teacher = TeacherImpl
+
+    case class SchoolImpl(teachers: Sequence[Teacher], courses: Sequence[Course])
+    opaque type School = SchoolImpl
+
+    extension (school: School) 
+      def setTeacherToCourse(teacher: Teacher, course: Course): School = 
+        teacherByName(teacher.name) match
+          case Optional.Just(t) => 
+            courseByName(course.name) match
+              //should it check also if the course is already present in teacher's courses?
+              //I do not check only because if they want me to do this check they should have used Set instead of Sequence
+              case Optional.Just(c) => SchoolImpl( 
+                Sequence.Cons(
+                  TeacherImpl(teacher.name, Sequence.Cons(c, teacher.courses)), 
+                  Sequence.filter(school.teachers)(_.name != t.name)), 
+                school.courses
+              )
+              case _ => school
+          case _ => school
+        
+      def nameOfCourse(course: Course): String = 
+        courseByName(course.name) match { case Optional.Just(c) => c.name; case Optional.Empty() => "" }
+      def addCourse(name: String): School = SchoolImpl(school.teachers, Sequence.Cons(CourseImpl(name), school.courses))
+      
+      def courseByName(name: String): Optional[Course] = 
+        Sequence.filter(school.courses)(c => c.name == name) match { case Sequence.Cons(h, t) => Optional.Just(h); case Sequence.Nil() => Optional.Empty() }
+      def addTeacher(name: String): School = SchoolImpl(Sequence.Cons(TeacherImpl(name, Sequence.Nil()), school.teachers), school.courses)
+      def coursesOfATeacher(teacher: Teacher): Sequence[Course] = 
+        teacherByName(teacher.name) match
+          case Optional.Just(t) => t.courses
+          case Optional.Empty() => Sequence.Nil()
+        
+      def teacherByName(name: String): Optional[Teacher] = 
+        Sequence.filter(school.teachers)(t => t.name == name) match { case Sequence.Cons(h, t) => Optional.Just(h); case Sequence.Nil() => Optional.Empty() }
+      def nameOfTeacher(teacher: Teacher): String = 
+        teacherByName(teacher.name) match { case Optional.Just(t) => t.name; case Optional.Empty() => "" }
---
+++ src/test/scala/tasks/adts/SchoolTest.scala
@@ -0,0 +1,13 @@
+package tasks.adts
+
+import org.junit.Test
+import org.junit.Assert.*
+import SchoolModel.*
+
+class SchoolTest {
+
+    val schoolModule: SchoolModule = SchoolModuleImpl
+    import schoolModule.* 
+
+
+}
